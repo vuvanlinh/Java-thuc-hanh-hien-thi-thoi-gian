3d5fd752ebdcb89e426ffa1a8a97aae50a087cea
vuvanlinh <vuvanlinh95@gmail.com>
2018-10-03 19:20:55 +0700

    System Time

---
+++ src/Thuchanhhienthithoigian.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class Thuchanhhienthithoigian {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.print("Date now : "+now);
+    }
+}
